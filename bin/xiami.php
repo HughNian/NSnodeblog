8c5bb206d1ba9055e3cb5494f80e0b43d89d5f41
HughNian <Hugh.Nian@163.com>
2014-01-02 15:03:56 +0800

    change xiami.php

--- bin/xiami.php
+++ bin/xiami.php
@@ -1,6 +1,6 @@
 <?php
 $key = urlencode($_GET['key']);
 $page = isset($_GET['page']) ? $_GET['page'] : 1;
-$url = "http://www.xiami.com/app/nineteen/search/key/{$key}/page/{$page}";
+$url = "http://www.xiami.com/app/nineteen/search/key/{$key}/diandian/1/page/{$page}?_=" . time();
 $json = file_get_contents($url);
 echo $json;
